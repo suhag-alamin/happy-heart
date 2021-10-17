8cd924e024094e27ca05c0bc5e75b4d3f42fa8cf
Suhag Al Amin <developersuhag@gmail.com>
2021-10-18 01:33:06 +0600

    404 page

--- src/components/PageNotFound/PageNotFound.js
+++ src/components/PageNotFound/PageNotFound.js
@@ -1,9 +1,22 @@
 import React from "react";
+import { Button } from "react-bootstrap";
+import { useHistory } from "react-router";
+import error from "../../images/404 Error Page not Found .gif";
 
 const PageNotFound = () => {
+  // handle button click
+  const history = useHistory();
+  const handleClick = () => {
+    history.push("/");
+  };
   return (
-    <div>
-      <h2>page not found</h2>
+    <div className="text-center">
+      <img src={error} alt="" />
+      <div>
+        <Button onClick={handleClick} className="happy-btn btn-danger">
+          Go Back to Home
+        </Button>
+      </div>
     </div>
   );
 };
